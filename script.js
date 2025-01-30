ad0a70baf3c019bddaa8001556f0940479f1feea
Sparsh-Jain-531 <sparshjain531@gmail.com>
2025-01-30 09:38:36 +0530

    Refactor button event listeners to use a common update function for calculation

--- script.js
+++ script.js
@@ -2,69 +2,59 @@ console.log("Welcome To CalcMate")
 
 let calculation='';
 
-document.getElementById("btn1").addEventListener("click", ()=>{
-    calculation += "1"
+const updateCalculation=(value)=>{
+    calculation+=value
     console.log(calculation)
+}
+
+document.getElementById("btn1").addEventListener("click", ()=>{
+    updateCalculation("1")
 })
 document.getElementById("btn2").addEventListener("click", ()=>{
-    calculation += "2"
-    console.log(calculation)
+    updateCalculation("2")
 })
 document.getElementById("btn3").addEventListener("click", ()=>{
-    calculation += "3"
-    console.log(calculation)
+    updateCalculation("3")
 })
 document.getElementById("btn5").addEventListener("click", ()=>{
-    calculation += "4"
-    console.log(calculation)
+    updateCalculation("4")
 })
 document.getElementById("btn6").addEventListener("click", ()=>{
-    calculation += "5"
-    console.log(calculation)
+    updateCalculation("5")
 })
 document.getElementById("btn7").addEventListener("click", ()=>{
-    calculation += "6"
-    console.log(calculation)
+    updateCalculation("6")
 })
 document.getElementById("btn9").addEventListener("click", ()=>{
-    calculation += "7"
-    console.log(calculation)
+    updateCalculation("7")
 })
 document.getElementById("btn10").addEventListener("click", ()=>{
-    calculation += "8"
-    console.log(calculation)
+    updateCalculation("8")
 })
 document.getElementById("btn11").addEventListener("click", ()=>{
-    calculation += "9"
-    console.log(calculation)
+    updateCalculation("9")
 })
 document.getElementById("btn4").addEventListener("click", ()=>{
-    calculation += " + "
-    console.log(calculation)
+    updateCalculation(" + ")
 })
 document.getElementById("btn8").addEventListener("click", ()=>{
-    calculation += " - "
-    console.log(calculation)
+    updateCalculation(" - ")
 })
 document.getElementById("btn12").addEventListener("click", ()=>{
-    calculation += " * "
-    console.log(calculation)
+    updateCalculation(" * ")
 })
 document.getElementById("btn13").addEventListener("click", ()=>{
-    calculation += "0 "
-    console.log(calculation)
+    updateCalculation("0")
 })
 document.getElementById("btn14").addEventListener("click", ()=>{
-    calculation += "."
-    console.log(calculation)
+    updateCalculation(".")
 })
 document.getElementById("btn15").addEventListener("click", ()=>{
     calculation = eval(calculation)
     console.log(calculation)
 })
 document.getElementById("btn16").addEventListener("click", ()=>{
-    calculation += " / "
-    console.log(calculation)
+    updateCalculation(" / ")
 })
 document.getElementById("btn17").addEventListener("click", ()=>{
     calculation = "";
